fe47c763ac5c53d46ed68f2aa2a0883ce0010b84
luhui <luhui@homolo.com>
2023-07-05 10:56:56 +0800

    fix: 主题切换测试版

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,22 +1,22 @@
-import { h } from 'vue'
-import { useData } from 'vitepress'
+import {h} from 'vue'
+import {useData} from 'vitepress'
 import Theme from 'vitepress/theme'
 import './style.scss'
-import './randomColors'
+import  './randomColors'
 import TopVisitRecord from '../components/TopVisitRecord.vue';
 
 export default {
-  ...Theme,
-  Layout: () => {
-    const props: Record<string, any> = {}
-    const { frontmatter } = useData()
+    ...Theme,
+    Layout: () => {
+        const props: Record<string, any> = {}
+        const {frontmatter} = useData()
 
-    // 添加自定义样式选择
-    if (frontmatter.value?.layoutClass) {
-      props.class = frontmatter.value.layoutClass
-    }
-    return h(Theme.Layout, props, {
-      'nav-bar-title-after': () => h(TopVisitRecord),
-    })
-  },
+        // 添加自定义样式选择
+        if (frontmatter.value?.layoutClass) {
+            props.class = frontmatter.value.layoutClass
+        }
+        return h(Theme.Layout, props, {
+            'nav-bar-title-after': () => h(TopVisitRecord),
+        })
+    },
 }
